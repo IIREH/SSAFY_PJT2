8025a2c638b67b0bc0511fa0b73a26dbc9ffac50
dohyun <dodohyun0807@gmail.com>
2022-03-28 10:26:45 +0900

    FEAT : contract페이지 에서 승인해야하는 부분 추가 구현(승인에 대한 상태값)

--- frontend/src/template/contractViewPage/index.jsx
+++ frontend/src/template/contractViewPage/index.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
 import { Navbar } from 'components/molecules';
+import { PDFReader } from 'reactjs-pdf-reader';
+import Styled from './styled';
+import { Heading } from '@/components/atoms';
+import Button from '@material-ui/core/Button';
+import router from 'next/router';
 
 const ContractViewPageTemplate = ({ contractId }) => {
+  // 승인해야하는 계약인지 상태값 부여
+  const isApprove = false;
+
+  const confirm = () => {
+    console.log('api요청');
+    alert('계약을 승인했습니다');
+    router.replace('/contractpage');
+  };
+
   return (
     <>
       <Navbar />
-      <h1>pdf 뷰어 및 계약 정보 보여주는 칸</h1>
-      {contractId}
+      <Styled.MainContainer>
+        <div>
+          <Heading>계약이름</Heading>
+          <Styled.ArticleArea>
+            <span>계약자</span>
+            <span>일자</span>
+          </Styled.ArticleArea>
+        </div>
+        <div style={{ overflow: 'scroll', height: 600 }}>
+          <PDFReader url={'http://www.pdf995.com/samples/pdf.pdf'} />
+        </div>
+        {isApprove ? (
+          <></>
+        ) : (
+          <Button variant="contained" color="primary" disableElevation onClick={confirm}>
+            승인
+          </Button>
+        )}
+      </Styled.MainContainer>
     </>
   );
 };
