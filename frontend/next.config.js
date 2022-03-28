3755635c708a5c2639c59ddedaba8940dceb5bc5
dohyun <dodohyun0807@gmail.com>
2022-03-28 10:27:29 +0900

    DOCS : pdf관련 라이브러리 설치

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
     config.plugins.push(new Dotenv({ silent: true }));
+    config.resolve.alias = Object.assign({}, config.resolve.alias, {
+      'react-pdf': 'react-pdf/dist/entry.noworker.js',
+    });
     return config;
   },
 };
