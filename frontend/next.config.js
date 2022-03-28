983a176f4eac2c1e83d709bc25c49cc8764fc14e
dohyun <dodohyun0807@gmail.com>
2022-03-28 16:47:12 +0900

    DOCS : 불필요 코드 삭제

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,8 @@ module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
     config.plugins.push(new Dotenv({ silent: true }));
-    config.resolve.alias = Object.assign({}, config.resolve.alias, {
-      'react-pdf': 'react-pdf/dist/entry.noworker.js',
-    });
+
     return config;
   },
+  pageExtensions: ['jsx', 'js'],
 };
