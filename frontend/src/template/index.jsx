f912091fc31d4b9adb894145d70e5a7d98fda1f3
dohyun <dodohyun0807@gmail.com>
2022-03-28 16:45:48 +0900

    DOCS : 불필요 코드 삭제

--- frontend/src/template/index.jsx
+++ frontend/src/template/index.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { useRecoilState } from 'recoil';
-import { userState } from '@/states';
-
 import { Navbar } from '@/components/molecules';
 
 const index = () => {
-  // recoil 사용 예시
-  const [user, setUser] = useRecoilState(userState);
   // react쿼리 예시
   const { isLoading, error, data } = useQuery('repoData', () =>
     fetch('https://api.github.com/repos/tannerlinsley/react-query').then((res) => res.json()),
@@ -16,13 +11,6 @@ const index = () => {
   if (isLoading) return 'Loading...';
 
   if (error) return 'An error has occurred: ' + error.message;
-
-  const addBtn = (e) => {
-    setUser((user) => ({
-      ...user,
-      name: user.name + '1',
-    }));
-  };
 
   return (
     <>
@@ -33,8 +21,6 @@ const index = () => {
         <strong>👀 {data.subscribers_count}</strong> <strong>✨ {data.stargazers_count}</strong>{' '}
         <strong>🍴 {data.forks_count}</strong>
       </div>
-      {user.name}
-      <button onClick={addBtn}>추가버튼</button>
     </>
   );
 };
