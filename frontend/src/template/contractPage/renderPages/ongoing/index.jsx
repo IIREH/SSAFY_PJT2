d514308d30d68b67d6016bec1dac0394844fdd46
dohyun <dodohyun0807@gmail.com>
2022-03-29 10:03:13 +0900

    DOCS : material List style 부분 분리

--- frontend/src/template/contractPage/renderPages/ongoing/index.jsx
+++ frontend/src/template/contractPage/renderPages/ongoing/index.jsx
@@ -11,6 +11,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
 import { useRouter } from 'next/router';
+import Styled from './styled';
 
 function createData(id, name, date, users) {
   return { id, name, date, users };
@@ -103,38 +104,9 @@ EnhancedTableHead.propTypes = {
   rowCount: PropTypes.number.isRequired,
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  paper: {
-    width: '90%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-  tableRow: {
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-}));
-
 const Ongoing = () => {
   const router = useRouter();
-  const classes = useStyles();
+  const classes = Styled.useStyles();
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [page, setPage] = React.useState(0);
