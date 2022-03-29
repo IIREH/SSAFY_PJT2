5f9e09929fdafacb07ed72317ce4e2ce8a746c95
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-03-29 15:22:00 +0900

    REFACTOR swagger 관련 path 인증 제외 처리

--- backend/chaintract/src/main/java/com/ssafy/chaintract/config/WebConfig.java
+++ backend/chaintract/src/main/java/com/ssafy/chaintract/config/WebConfig.java
@@ -7,13 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
+        /**
+         * swagger path : 개발 끝나고 배포전에 지워야함
+         * "/swagger-ui/**","/swagger-resources/**","/v3/api-docs"
+         * */
+
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/auth/login");
+                .excludePathPatterns("/","/auth/login","/swagger-ui/**","/swagger-resources/**","/v3/api-docs");
     }
-
 }
