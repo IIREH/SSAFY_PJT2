d4f6bb6a6dbf058f3a5f3a5c92af46b1e35ef1ff
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-01 11:50:14 +0900

    FIX Session 관련 에러 수정

--- backend/chaintract/src/main/java/com/ssafy/chaintract/domain/ContractState.java
+++ backend/chaintract/src/main/java/com/ssafy/chaintract/domain/ContractState.java
@@ -3,11 +3,4 @@ package com.ssafy.chaintract.domain;
 public enum ContractState {
     ONGOING,
     COMPLETE;
-
-//    ContractState() {}
-
-
-//    public ContractState completeContract() {
-//        return COMPLETE();
-//    }
 }
